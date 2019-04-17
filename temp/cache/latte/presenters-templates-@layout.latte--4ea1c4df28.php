00d56e32d6a276f5dc3006f3709d498f1644d4f7
Fleg <41325688+FlegelovaBarbora@users.noreply.github.com>
2019-04-17 20:45:55 +0200

    Add files via upload

---
+++ temp/cache/latte/presenters-templates-@layout.latte--4ea1c4df28.php
@@ -0,0 +1,81 @@
+<?php
+// source: C:\wamp64\www\nette-calculator\app\presenters/templates/@layout.latte
+
+use Latte\Runtime as LR;
+
+class Template4ea1c4df28 extends Latte\Runtime\Template
+{
+	public $blocks = [
+		'scripts' => 'blockScripts',
+	];
+
+	public $blockTypes = [
+		'scripts' => 'html',
+	];
+
+
+	function main()
+	{
+		extract($this->params);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+
+	<title><?php
+		if (isset($this->blockQueue["title"])) {
+			$this->renderBlock('title', $this->params, function ($s, $type) {
+				$_fi = new LR\FilterInfo($type);
+				return LR\Filters::convertTo($_fi, 'html', $this->filters->filterContent('striphtml', $_fi, $s));
+			});
+			?> | <?php
+		}
+?>Nette Web</title>
+</head>
+
+<body>
+<?php
+		$iterations = 0;
+		foreach ($flashes as $flash) {
+			?>	<div<?php if ($_tmp = array_filter(['flash', $flash->type])) echo ' class="', LR\Filters::escapeHtmlAttr(implode(" ", array_unique($_tmp))), '"' ?>><?php
+			echo LR\Filters::escapeHtmlText($flash->message) /* line 11 */ ?></div>
+<?php
+			$iterations++;
+		}
+?>
+
+<?php
+		$this->renderBlock('content', $this->params, 'html');
+?>
+
+<?php
+		if ($this->getParentName()) return get_defined_vars();
+		$this->renderBlock('scripts', get_defined_vars());
+?>
+</body>
+</html>
+<?php
+		return get_defined_vars();
+	}
+
+
+	function prepare()
+	{
+		extract($this->params);
+		if (!$this->getReferringTemplate() || $this->getReferenceType() === "extends") {
+			if (isset($this->params['flash'])) trigger_error('Variable $flash overwritten in foreach on line 11');
+		}
+		Nette\Bridges\ApplicationLatte\UIRuntime::initialize($this, $this->parentName, $this->blocks);
+		
+	}
+
+
+	function blockScripts($_args)
+	{
+?>	<script src="https://nette.github.io/resources/js/3/netteForms.min.js"></script>
+<?php
+	}
+
+}
---
+++ temp/cache/nette.robotLoader/9890858815460e7f8ef344f28960f01c.php
@@ -0,0 +1,36 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Model\\CalculatorManager' => 
+    array (
+      'file' => 'C:\\wamp64\\www\\nette-calculator\\app\\model\\CalculatorManager.php',
+      'time' => 1555522242,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      'file' => 'C:\\wamp64\\www\\nette-calculator\\app\\presenters\\Error4xxPresenter.php',
+      'time' => 1554368491,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      'file' => 'C:\\wamp64\\www\\nette-calculator\\app\\presenters\\ErrorPresenter.php',
+      'time' => 1554368491,
+    ),
+    'App\\RouterFactory' => 
+    array (
+      'file' => 'C:\\wamp64\\www\\nette-calculator\\app\\router\\RouterFactory.php',
+      'time' => 1554450568,
+    ),
+    'App\\Presenters\\CalculatorPresenter' => 
+    array (
+      'file' => 'C:\\wamp64\\www\\nette-calculator\\app\\presenters\\CalculatorPresenter.php',
+      'time' => 1555526015,
+    ),
+  ),
+  1 => 
+  array (
+    'App\\IRouter' => 3,
+    'Nette\\Environment' => 3,
+  ),
+);
